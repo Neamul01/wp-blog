df93599f9b0725d27f2298c4a06dc982ddbbdf60
Neamul01 <neamulhaque2002@gmail.com>
2023-05-08 17:34:51 +0600

    walker menu added

--- functions.php
+++ functions.php
@@ -77,3 +77,12 @@ add_action('customize_register', 'noman_customizer_register');
 
 // Menu Register
 register_nav_menu( 'main_menu', __('Main Menu', 'blogsite') );
+
+// Walker manu prooperties 
+function noman_nav_description($item_output, $item, $args){
+    if(!empty ($item->description)){
+        $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' . $args->link_after . '</a>' , $item_output );
+    }
+    return $item_output;
+};
+add_filter('walker_nav_menu_start_el', 'noman_nav_description', 10, 3 );
