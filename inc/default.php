765728a615e99176a8c4b178aa6d2617e8cc33c3
Neamul01 <neamulhaque2002@gmail.com>
2023-05-09 10:44:36 +0600

    blog posts added

--- inc/default.php
+++ inc/default.php
@@ -1,3 +1,7 @@
 <?php
 // theme title
 add_theme_support( 'title-tag' );
+
+// theme thumbnail
+add_theme_support( 'post-thumbnails', array('page', 'post') );
+add_image_size( 'post-thumbnails', 970, 350, true );
--- index.php
+++ index.php
@@ -8,8 +8,36 @@ get_header();
     <div id="body_area">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <?php the_content(); ?>
+                <div class="col-md-9">
+                    
+                    <?php 
+                        if(have_posts()) :
+                        while(have_posts()): the_post();
+                    ?>
+
+                    <div class="blog_area">
+                        <div class="post_thumb">
+                        <a href="<?php the_permalink(  ) ?>"><?php the_post_thumbnail( 'post-thumbnails' ); ?></a>
+                            
+                        </div>
+                        <div class="post_details">
+                            <h2><a href="<?php the_permalink(  ) ?>"><?php the_title()?></a></h2>
+                            <?php the_excerpt(); ?>
+                        </div>
+
+                    </div>
+
+                    <?php
+                        endwhile;
+                        else :
+                            _e('No post found');
+                        endif;
+                    ?>
+
+                    
+                </div>
+                <div class="col-md-3">
+                    <h2>The sidebar</h2>
                 </div>
             </div>
         </div>
