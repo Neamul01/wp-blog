8267dc3b3701e7f1086cd9d43c87bd4fb48fd24e
Neamul01 <neamulhaque2002@gmail.com>
2023-05-09 11:06:54 +0600

    excerpt and more, length button added

--- inc/default.php
+++ inc/default.php
@@ -5,3 +5,14 @@ add_theme_support( 'title-tag' );
 // theme thumbnail
 add_theme_support( 'post-thumbnails', array('page', 'post') );
 add_image_size( 'post-thumbnails', 970, 350, true );
+
+// exerpt more
+function noman_excerpt_more($more){
+    return '</br> <a class="read_more" href="'.get_permalink( $post->ID ) . '"/>' . 'Read More...' . '</a>';
+}
+add_filter( 'excerpt_more', 'noman_excerpt_more' );
+
+function noman_excerpt_length($length){
+    return 20;
+}
+add_filter( 'excerpt_length', 'noman_excerpt_length', 999 );
