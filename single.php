c56f357cd90bb41d6707e02a4b5ab9f76e32b92a
Neamul01 <neamulhaque2002@gmail.com>
2023-05-15 16:39:32 +0600

    page, post added

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-*this template for displaying the header
+*this template for displaying single post
 */
 get_header();
 ?>
@@ -9,7 +9,11 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-md-9">
-                    <?php get_template_part( 'template_part/blog_setup' ) ?>
+                <?php get_template_part( 'template_part/post_setup' ) ?>
+
+                <div id="comments_area">
+                    <?php comments_template(); ?>
+                </div>
                 </div>
                 <div class="col-md-3">
                     <?php get_sidebar(); ?>
